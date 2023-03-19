65e3cfd198b4c4ebf11e8e0699013858d159d50e
skolanko_ubuntu <skolanko222@gmail.com>
2023-03-19 20:09:54 +0100

    slider implemented

--- grafika/lab/lab2/main.cpp
+++ grafika/lab/lab2/main.cpp
@@ -1,138 +1,29 @@
 //Uwaga! Co najmniej C++17!!!
 //Project-> ... Properties->Configuration Properties->General->C++ Language Standard == ISO C++ 17 Standard (/std:c++17)
+#define OFFSET 200
+
+#define DEFAULT_WIDTH 800
+#define DEFAULT_HEIGHT 600
+
+#define DEFAULT_SLIDER_HEIGHRT 40
 
 #include "SFML/Graphics.hpp"
 #include "ShareTechMono-Regular.h"
+#include "slider.h"
+#include "things.h"
 #include <numeric>
 #include <iostream>
 #include <memory>
 #include <cmath>
 
+
 //Tak – dobrze państwo widzą – TO jest zmienna globalna! Czytanie i przetwarzanie fontów w SFML jest bardzo kosztowne. Dlatego zrobimy to raz. 
 //Co nie zmienia faktu, że można by to zrobić bez zmiennej globalnej i to całkiem ładnie. Jak? To już dla Państwa :-)
 std::shared_ptr<sf::Font> font;
 
-class hexagon : public sf::Drawable
-{
-protected:
-    sf::Vector2f left_top, right_bottom, center;
-    sf::Vector2f p[6]; // Kolejność punktów opisana w pliku PDF do laboratorium.
-public:
-    void Set_Borders(sf::Vector2f _left_top, sf::Vector2f _right_bottom); // oblicza pozycję sześciokątów
-    void Draw_Border(sf::RenderTarget& target, sf::RenderStates states, sf::String name) const;
-    bool rhombus(sf::Vector2f p0, sf::Vector2f p1, sf::Vector2f p, float& alpha, float& beta) const;
-private:
-    float d_coefficient(sf::Vector2f p0, sf::Vector2f p1, sf::Vector2f p) const;
-};
-
-//Trochę matematyki jeszcze nikomu nie zaszkodziło. Więc dwie kolejne metody to czysta matematyka.
-float hexagon::d_coefficient(sf::Vector2f p0, sf::Vector2f p1, sf::Vector2f p) const
-{
-    float A = p0.y - p1.y;
-    float B = p1.x - p0.x;
-    float C = p0.x * (p1.y - p0.y) - p0.y * (p1.x - p0.x);
-
-    return 2 * ((A * p.x + B * p.y + C) / sqrt(A * A + B * B)) / sqrt(3.0f);
-}
-
-bool hexagon::rhombus(sf::Vector2f p0, sf::Vector2f p1, sf::Vector2f p, float& alpha, float& beta) const
-{
-    float L = sqrt((p1.x - p0.x) * (p1.x - p0.x) + (p1.y - p0.y) * (p1.y - p0.y));
-    sf::Vector2f p3(0.5f * (p0.x - p1.x) + 0.5f * sqrt(3.0f) * (p1.y - p0.y) + p0.x, 0.5f * sqrt(3.0f) * (p0.x - p1.x) - 0.5f * (p1.y - p0.y) + p0.y);
-
-    beta = -d_coefficient(p0, p1, p) / L;
-    alpha = d_coefficient(p0, p3, p) / L;
-
-    if ((alpha > 1.0f) || (beta > 1.0f) || (alpha < 0.0f) || (beta < 0.0f)) return false; else return true;
-}
-
-void hexagon::Set_Borders(sf::Vector2f _left_top, sf::Vector2f _right_bottom)
-{
-    left_top = _left_top;
-    right_bottom = _right_bottom;
-    float a = ((right_bottom.y - left_top.y) / 2.0f + 0.5f) - 20.0f;
-    center = left_top + sf::Vector2f((right_bottom.x - left_top.x) / 2.0f + 0.5f, (right_bottom.y - left_top.y) / 2.0f + 0.5f + 10);
-
-    p[0] = center - sf::Vector2f( 0.0f, a);
-    p[1] = center - sf::Vector2f(0.5f * sqrt(3.0f) * a, 0.5f * a);
-    p[2] = center - sf::Vector2f(0.5f * sqrt(3.0f) * a, -0.5f * a);
-    p[3] = center + sf::Vector2f(0.0f, a);
-    p[4] = center + sf::Vector2f(0.5f * sqrt(3.0f) * a, 0.5f * a);
-    p[5] = center + sf::Vector2f(0.5f * sqrt(3.0f) * a, -0.5f * a);
-}
-
-void hexagon::Draw_Border(sf::RenderTarget& target, sf::RenderStates states, sf::String name) const
-{
-    //Rysowanie obramowania sześciokąta.
-	sf::VertexArray lines(sf::LinesStrip, 7);
-	for(int i = 0; i < 7; i++) 
-	{
-		lines[i].color = sf::Color::Black;
-		lines[i].position = p[i % 6];
-	}
-	target.draw(lines, states);
-	sf::RectangleShape rec;
- rec.setPosition(center);
- //rec.setSize(sf::Vector2f());
-
-
-}
-
-class hexagon_RGB : public hexagon
-{
-public:
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-};
-
-void hexagon_RGB::draw(sf::RenderTarget& target, sf::RenderStates states) const
-{
-    //Tu trzeba narysować sześciokąt RGB.
-
-    Draw_Border(target, states, "RGB");
-}
-
-class hexagon_CMY : public hexagon
-{
-public:
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-};
-
-void hexagon_CMY::draw(sf::RenderTarget& target, sf::RenderStates states) const
-{
-    //Tu trzeba narysować sześciokąt CMY.
-
-    Draw_Border(target, states, "CMY");
-}
-
-class hexagon_HSL : public hexagon
-{
-public:
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-};
-
-void hexagon_HSL::draw(sf::RenderTarget& target, sf::RenderStates states) const
-{
-    //Tu trzeba narysować sześciokąt HSL.
-    
-    Draw_Border(target, states, "HSL");
-}
-
-class hexagon_HSB : public hexagon
-{
-public:
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-};
-
-void hexagon_HSB::draw(sf::RenderTarget& target, sf::RenderStates states) const
-{
-    //Tu trzeba narysować sześciokąt HSB.
-
-    Draw_Border(target, states, "HSB");
-}
-
 int main()
 {
- sf::RenderWindow window(sf::VideoMode(800, 600), "GFK Lab 02", sf::Style::Titlebar | sf::Style::Close | sf::Style::Resize);
+ sf::RenderWindow window(sf::VideoMode(DEFAULT_WIDTH, DEFAULT_HEIGHT), "GFK Lab 02", sf::Style::Titlebar | sf::Style::Close | sf::Style::Resize);
  
  hexagon_RGB h_RGB;
  hexagon_CMY h_CMY;
@@ -158,6 +49,7 @@ int main()
  h_HSB.Set_Borders(sf::Vector2f(300.0f, 294.0f), sf::Vector2f(600.0f, 588.0f));
 
  around_half_secound_clock.restart();
+ Slider right_slider(DEFAULT_WIDTH,DEFAULT_HEIGHT);
 
  while (window.isOpen())
  {
@@ -168,15 +60,29 @@ int main()
 
   while (window.pollEvent(event))
   {
+    sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
    if (event.type == sf::Event::Closed) window.close();
-	  else if(event.type == sf::Event::Resized)
+	else if(event.type == sf::Event::Resized)
 	  {
-     h_RGB.Set_Borders(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(300.0f,294.0f));
-     h_CMY.Set_Borders(sf::Vector2f(300.0f, 0.0f), sf::Vector2f(600.0f, 294.0f));
-     h_HSL.Set_Borders(sf::Vector2f(0.0f, 294.0f), sf::Vector2f(300.0f, 588.0f));
-     h_HSB.Set_Borders(sf::Vector2f(300.0f, 294.0f), sf::Vector2f(600.0f, 588.0f));
-
+        float width = static_cast<float>(event.size.width);
+        float height = static_cast<float>(event.size.height);
+        window.setView(sf::View(sf::FloatRect(0, 0, event.size.width, event.size.height)));
+        
+        h_RGB.Set_Borders(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(min(width - OFFSET,height)/2.,min(width - OFFSET,height)/2.));
+        h_CMY.Set_Borders(sf::Vector2f(min(width - OFFSET,height) /2., 0.0f), sf::Vector2f(width/2.+min(width - OFFSET,height)/2., min(width - OFFSET,height)/2.));
+        
+        h_HSL.Set_Borders(sf::Vector2f(0.0f, min(width - OFFSET,height)/2.), sf::Vector2f(min(width - OFFSET,height)/2.,min(width - OFFSET,height)));
+        h_HSB.Set_Borders(sf::Vector2f(min(width - OFFSET,height) /2., min(width - OFFSET,height)/2.), sf::Vector2f(width/2.+ min(width - OFFSET,height)/2., min(width - OFFSET,height)));
+        
+        right_slider.setParams(width,height);
+ 
 	  }
+    else if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    {
+        double a = right_slider.leftButtonPressed(mouse_pos);
+        std::cout << a << std::endl;
+    }
+
       // Tu trzeba obsłużyć zdarzenia: zmianę rozmiaru okna oraz klikanie w „suwaczek”. 
   }
 
@@ -185,6 +91,7 @@ int main()
   window.draw(h_HSL);
   window.draw(h_HSB);
   // Pewnie tu gdzieś wypadało by dorysować jeszcze suwaczek... 
+  window.draw(right_slider);
   text.setString(std::to_string(mean_frames_time)+ "us");
   window.draw(text);
 
