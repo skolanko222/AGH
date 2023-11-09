c688946c408709c68a77e0691e0beb3195515017
skolanko222 <skolanko222@gmail.com>
2023-11-09 23:55:37 +0100

    java lab4_c

---
+++ java/cwiczenia/lab3_d/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+            int[] tab1 = new int[6];
+            String [] tab2 = {"abs"};
+            // convert string to array
+            String str = "abc\n";
+            char[] ch = str.toCharArray();
+            System.out.println(ch[2]);
+
+            znak[] tab3 = new znak[3];
+            for(znak i : tab3){
+                i = new znak();
+                System.out.println(i.n);
+            }
+    }
+}
